66f48fdcd706bb8058f15bc49258003bd3238c20
Stanislav Gryaznov <st.gryaznov@gmail.com>
2023-07-05 16:08:00 +0300

    Add a simple test to check it works.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::add;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(4, add(2, 2));
+    }
+}
